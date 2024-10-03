8f443dc3eea92f28a786150090609819c51996c5
ankit-2112 <srcankit211299@gmail.com>
2024-10-03 12:18:33 +0530

    update social buttons

--- JS/social_handling.js
+++ JS/social_handling.js
@@ -5,13 +5,25 @@ let githubuButton = document.getElementById("github");
 
 gmailButton.addEventListener("click",function(){
     console.log("gmail clicked");
+    const email = "ankitgupta211299@gmail.com";
+    const subject = "Hello Ankit";
+    const body = "I would like to get in touch";
+
+    // Construct the mailto link
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+
+    // Redirect to the mailto link
+    window.open(mailtoLink,"_self");
 });
 linkedinButton.addEventListener("click",function(){
     console.log("linkedIn clicked");
+    window.open("https://www.linkedin.com/in/an-ki-it","_blank");
 });
 whatsappButton.addEventListener("click",function(){
     console.log("whatsapp clicked");
+    window.open("https://wa.link/gcbvfx","_blank");
 });
 githubuButton.addEventListener("click",function(){
     console.log("github clicked");
+    window.open("https://github.com/ankit-2112","_blank");
 });
